3aae882aa6bd711b745b8d9d6b682243201f4772
nilupul20 <nilupul.20@cse.mrt.ac.lk>
2023-08-30 10:21:27 +0530

    updated

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ app = FastAPI()
 
 @app.get("/get_summary/{url:path}")
 async def get_summary(url: str):
+    print("GPU: ",torch.cuda.is_available())
     docs = get_chunks(url)
-    summary = get_summary(docs)
+    summary = generate_summary(docs)
     return {"summary": summary}
-
 
 if __name__ == "__main__":
     uvicorn.run("main:app")
@@ -38,61 +38,67 @@ def get_chunks(url):
     docs = text_splitter.create_documents([text])
     for i in range(len(docs)):
         docs[i].page_content = docs[i].page_content.replace("\r\n\n\n", " ")
+    print("created %d chunks." % len(docs))
     return docs
 
-def get_summary(docs):
+def generate_summary(docs):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     tokenizer = AutoTokenizer.from_pretrained("pszemraj/led-large-book-summary")
     model = AutoModelForSeq2SeqLM.from_pretrained(
                 "pszemraj/led-large-book-summary", 
                 # load_in_8bit=True,
                 #low_cpu_mem_usage=True,
-    )
+    ).to(device)
     summarizer = pipeline(
         task = "summarization",
         model = model,
         tokenizer = tokenizer,
         pad_token_id = tokenizer.eos_token_id,
         max_length = 300,
-        temperature = 0.2,
-        device=0 if torch.cuda.is_available() else -1,
+        # temperature = 0.2,
+        device=device,
     )
 
+    print("Starting chunk summarization...")
     # chunk_summaries = {}
-    # text = ""
-    # for i in range(len(docs)):
-    #     chunk_summary = summarizer(
-    #             docs[i].page_content,
-    #             min_length=400,
-    #             max_length=1000,
-    #             no_repeat_ngram_size=3,
-    #             encoder_no_repeat_ngram_size=3,
-    #             repetition_penalty=3.5,
-    #             num_beams=4,
-    #             early_stopping=True,
-    #         )[0]['summary_text']
-    #     # chunk_summaries[i]= chunk_summary
-    #     text += (chunk_summary + "\n")
-    #
-    # final_summary = summarizer(
-    #             text,
-    #             min_length=500,
-    #             max_length=1200,
-    #             no_repeat_ngram_size=3,
-    #             encoder_no_repeat_ngram_size=3,
-    #             repetition_penalty=3.5,
-    #             num_beams=4,
-    #             early_stopping=True,
-    #         )[0]['summary_text']
+    initial_summary = ""
+    for i in range(len(docs)):
+        chunk_summary = summarizer(
+                docs[i].page_content,
+                min_length=400,
+                max_length=1000,
+                no_repeat_ngram_size=3,
+                encoder_no_repeat_ngram_size=3,
+                repetition_penalty=3.5,
+                num_beams=4,
+                early_stopping=True,
+            )[0]['summary_text']
+        print("chunk %d summerized." %i)
+        # chunk_summaries[i]= chunk_summary
+        initial_summary += (chunk_summary + "\n")
+    print("Chunk summarization completed.")
 
+    print("Generating final summary")
     summary = summarizer(
-        docs[1].page_content,
-        # final_summary,
-        min_length=200,
-        max_length=400,
-        no_repeat_ngram_size=3,
-        encoder_no_repeat_ngram_size=3,
-        repetition_penalty=3.5,
-        num_beams=4,
-        early_stopping=True,
-    )[0]['summary_text']
+                initial_summary,
+                min_length=500,
+                max_length=1200,
+                no_repeat_ngram_size=3,
+                encoder_no_repeat_ngram_size=3,
+                repetition_penalty=3.5,
+                num_beams=4,
+                early_stopping=True,
+            )[0]['summary_text']
+    print("Final summary generated.")
+    # summary = summarizer(
+    #     docs[1].page_content,
+    #     # final_summary,
+    #     min_length=200,
+    #     max_length=400,
+    #     no_repeat_ngram_size=3,
+    #     encoder_no_repeat_ngram_size=3,
+    #     repetition_penalty=3.5,
+    #     num_beams=4,
+    #     early_stopping=True,
+    # )[0]['summary_text']
     return summary
